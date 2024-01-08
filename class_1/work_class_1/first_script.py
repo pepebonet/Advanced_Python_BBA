69f33b4348939cfbcd045d43b0a20a57875af79f
Pepe <pepebogi5@gmail.com>
2024-01-08 18:05:57 +0100

    add all work of class day 1

---
+++ class_1/work_class_1/first_script.py
@@ -0,0 +1,15 @@
+"""
+Script to introduce scripts
+"""
+import pandas as pd
+
+def main():
+    """
+    Main function to perform a given task
+    """
+    print('Hello I am in the main function')
+
+
+if __name__ == "__main__":
+    print('Hello I come here first')
+    main()
